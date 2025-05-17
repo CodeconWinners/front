bef5f14a0db0e942674aab24be7653b470a1b3f9
Mateus Veloso <mateus.13061903@gmail.com>
2025-05-17 21:22:12 +0000

    Adicionando novo client

--- src/shared/services/CalendarService.ts
+++ src/shared/services/CalendarService.ts
@@ -5,11 +5,11 @@ import type { EventsDto } from "../dtos/EventsDto"
 export const CalendarService = (fetch: Axios) => {
 
     return {
-        getEvents(date: string): AxiosPromise<{itens: EventsDto[]}> {
+        getEvents(date: string): AxiosPromise<{items: EventsDto[]}> {
             return fetch.get('/calendar/read-events', {
                 params: {
-                    date: date,
-                    userId: "abdshvahdvvbdas"
+                    // date: date,
+                    userId: "8c615822-e477-4af4-90b5-3b952a086d75"
                 }
             })
         }
