97880e71b91fbdfbd1b3398ad254f6e0e2357271
Mateus Veloso <mateus.13061903@gmail.com>
2025-05-17 22:47:21 +0000

    adiciona googleCode

--- src/features/login/LoginComponentFunctions.tsx
+++ src/features/login/LoginComponentFunctions.tsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 
 export const useLoginComponentFunctions = () => {
+    const [searchParams] = useSearchParams()
     const [scale, setScale] = useState('full');
+    const googleCode = searchParams.get('code')
 
     useEffect(() => {
         setTimeout(() => {
             setScale('0')
         }, 520)
     }, [])
+
+    useEffect(() => {
+        console.log({googleCode})
+    },[googleCode])
     return {
         scale
     }
