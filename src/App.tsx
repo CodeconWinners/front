f4f2cae15c893ad456c2bcf8e861ffafd1c3c373
Vitor Alcântara <costec222@gmail.com>
2025-05-17 11:43:46 -0300

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/ui/button"
 
 
 function App() {
@@ -5,6 +6,9 @@ function App() {
   return (
     <>
       <h1 className='text-xl font-bold underline'>Hello World</h1>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
     </>
   )
 }
