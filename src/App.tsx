9e3e4c11d33de23430f0d00b6f8eab67b19513e5
Mateus Veloso <mateus.13061903@gmail.com>
2025-05-17 18:01:39 +0000

    adiciona calendario

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { Button } from "./components/ui/button"
+import { CalendarComponent } from "./shared/components/calendar/CalendarComponent"
 
 
 function App() {
 
   return (
     <>
-      <h1 className='text-xl font-bold underline'>Hello World</h1>
-      <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
     </>
   )
 }
---
+++ src/shared/enums/MeetingStatusEnum.ts
@@ -0,0 +1,6 @@
+export enum MeetingStatusEnum {
+    MUITO_UTIL = 1,
+    UTIL = 2,
+    INUTIL = 3,
+    IMPRATICAVEL = 4
+};
